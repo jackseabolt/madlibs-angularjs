1778b1f4c41c8d3e3a3389dd4cc457109f596c2b
John Seabolt <johnseabolt@Johns-MacBook-Pro.local>
2018-04-10 15:23:52 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+angular.module('myApp', [])
+.controller('myController', ['$scope', function myController($scope) {
+    $scope.female_name = "FEMALE NAME"; 
+    $scope.position = "POSITION"; 
+    $scope.tedious_task = "TEDIOUS TASK"; 
+    $scope.dirty_task = "DIRTY TASK"; 
+    $scope.female_name = "FEMALE NAME"; 
+    $scope.celebrity = "CELEBRITY"; 
+    $scope.useless_skill = "USELESS SKILL"; 
+}]); 
